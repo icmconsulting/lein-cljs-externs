3d5db9ab73d302b6ee070aaebdc8c9578dc8d90b
Brendan Bates <batesbrendan@gmail.com>
2014-07-24 23:50:50 +1000

    Add readme

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-cljs-externs "0.1.0"
   :description "Generate js externs from cljs metadata"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/bbbates/lein-cljs-externs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
